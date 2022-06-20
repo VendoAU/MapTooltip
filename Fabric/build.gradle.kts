9b50f0153b125a14a685c3c26bc121a633c425cc
Anthony Hilyard <anthony.hilyard@gmail.com>
2022-06-19 22:19:06 -0700

    Changed mixin to Fabric's TooltipComponentCallback.

--- Fabric/build.gradle.kts
+++ Fabric/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 val minecraftVersion: String by project
 val yarnMappings: String by project
 val fabricLoaderVersion: String by project
+val fabricVersion: String by project
 
 version = "2.0.0"
 
@@ -16,6 +17,7 @@ dependencies {
     minecraft("com.mojang:minecraft:${minecraftVersion}")
     mappings("net.fabricmc:yarn:${yarnMappings}:v2")
     modImplementation("net.fabricmc:fabric-loader:${fabricLoaderVersion}")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${fabricVersion}")
 }
 
 tasks {
