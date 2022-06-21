39b9217c1e457c47da3610144bab0581aadd9532
VendoAU <reillyreaby@gmail.com>
2022-06-22 00:53:26 +1000

    Updated version

--- Fabric/build.gradle.kts
+++ Fabric/build.gradle.kts
@@ -7,7 +7,7 @@ val yarnMappings: String by project
 val fabricLoaderVersion: String by project
 val fabricVersion: String by project
 
-version = "2.0.0"
+version = "2.0.1"
 
 base {
     archivesName.set("maptooltip-fabric-${minecraftVersion}")
