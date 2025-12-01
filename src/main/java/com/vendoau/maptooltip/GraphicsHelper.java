e9b9c5cae69b977f7eec35261906bc1a818be4e0
VendoAU <reillyreaby@gmail.com>
2025-12-01 16:47:35 +1100

    1.19

--- src/main/java/com/vendoau/maptooltip/GraphicsHelper.java
+++ src/main/java/com/vendoau/maptooltip/GraphicsHelper.java
@@ -1,41 +1,69 @@
 package com.vendoau.maptooltip;
 
-import net.minecraft.client.gui.GuiGraphics;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.resources.ResourceLocation;
 
-//? > 1.21.2
+//? < 1.20
+/*import net.minecraft.client.gui.GuiComponent;*/
+
+//? >= 1.20
+import net.minecraft.client.gui.GuiGraphics;
+
+//? > 1.21.2 {
 import net.minecraft.client.renderer.RenderPipelines;
+import org.joml.Matrix3x2fStack;
+//?}
 
 public class GraphicsHelper {
 
+    //? >= 1.20
     private final GuiGraphics guiGraphics;
 
+    //? < 1.21.2 {
+    /*private final PoseStack pose;
+    *///?} else {
+    private final Matrix3x2fStack pose;
+     //?}
+
+    //? < 1.20 {
+    /*public GraphicsHelper(PoseStack pose) {
+        this.pose = pose;
+    }
+    *///?} else {
     public GraphicsHelper(GuiGraphics guiGraphics) {
         this.guiGraphics = guiGraphics;
+        pose = guiGraphics.pose();
     }
+    //?}
 
     public void push() {
         //? < 1.21.2 {
-        /*guiGraphics.pose().pushPose();
-         *///?} else {
-        guiGraphics.pose().pushMatrix();
-        //?}
+        /*pose.pushPose();
+        *///?} else {
+        pose.pushMatrix();
+         //?}
     }
 
     public void pop() {
         //? < 1.21.2 {
-        /*guiGraphics.pose().popPose();
-         *///?} else {
-        guiGraphics.pose().popMatrix();
-        //?}
+        /*pose.popPose();
+        *///?} else {
+        pose.popMatrix();
+         //?}
     }
 
     public void translate(float x, float y) {
-        guiGraphics.pose().translate(
+        translate(x, y, 1);
+    }
+
+    public void translate(float x, float y, float z) {
+        pose.translate(
                 x,
                 y
                 //? < 1.21.2
-                /*, 1*/
+                /*, z*/
         );
     }
 
@@ -44,7 +72,7 @@ public class GraphicsHelper {
     }
 
     public void scale(float x, float y) {
-        guiGraphics.pose().scale(
+        pose.scale(
                 x,
                 y
                 //? < 1.21.2
@@ -53,6 +81,13 @@ public class GraphicsHelper {
     }
 
     public void blit(ResourceLocation sprite, int x, int y, int width, int height) {
+        //? < 1.20 {
+        /*RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1, 1, 1, 1);
+        RenderSystem.setShaderTexture(0, sprite);
+        GuiComponent.blit(pose, x, y, 0, 0, width, height, width, height);
+        *///?} else {
+        push();
         guiGraphics.blit(
                 //? >= 1.21.2
                 RenderPipelines.GUI_TEXTURED,
@@ -66,5 +101,6 @@ public class GraphicsHelper {
                 width,
                 height
         );
+        //?}
     }
 }
--- src/main/java/com/vendoau/maptooltip/MapTooltipComponent.java
+++ src/main/java/com/vendoau/maptooltip/MapTooltipComponent.java
@@ -1,9 +1,12 @@
 package com.vendoau.maptooltip;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.Tesselator;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.tooltip.TooltipComponent;
 import net.minecraft.world.item.ItemStack;
@@ -11,30 +14,32 @@ import net.minecraft.world.item.MapItem;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.saveddata.maps.MapItemSavedData;
 
+//? >= 1.20
+import net.minecraft.client.gui.GuiGraphics;
+
 //? >= 1.21 {
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.level.saveddata.maps.MapId;
 //?}
 
-//? >= 1.21.2 {
+//? >= 1.21.2
 import net.minecraft.client.renderer.state.MapRenderState;
- //?}
 
 // TODO: Move this to common
 public class MapTooltipComponent implements ClientTooltipComponent, TooltipComponent {
 
     //? < 1.21 {
     /*private final ResourceLocation MAP_SPRITE = new ResourceLocation("textures/map/map_background.png");
-    *///?} else {
+     *///?} else {
     private final ResourceLocation MAP_SPRITE = ResourceLocation.withDefaultNamespace("textures/map/map_background.png");
     //?}
 
     private final Minecraft mc;
     //? < 1.20.5 {
     /*private final Integer id;
-    *///?} else {
+     *///?} else {
     private final MapId id;
-     //?}
+    //?}
     //? >= 1.21.2
     private final MapRenderState mapRenderState;
 
@@ -42,42 +47,62 @@ public class MapTooltipComponent implements ClientTooltipComponent, TooltipCompo
         mc = Minecraft.getInstance();
         //? < 1.20.5 {
         /*id = MapItem.getMapId(item);
-        *///?} else {
+         *///?} else {
         id = item.get(DataComponents.MAP_ID);
-         //?}
+        //?}
         //? >= 1.21.2
         mapRenderState = new MapRenderState();
     }
 
     @Override
     public void renderImage(Font font,
-                            int x,
-                            int y,
-                            //? >= 1.21.2 {
-                            int i1,
-                            int i2,
-                            //?}
-                            GuiGraphics guiGraphics) {
+                             int x,
+                             int y,
+                             //? < 1.20 {
+                             /*PoseStack pose,
+                             ItemRenderer itemRenderer,
+                             int i1
+                             *///?}
+                             //? >= 1.21.2 {
+                             int i1,
+                             int i2,
+                             //?}
+                             //? >= 1.20
+                             GuiGraphics guiGraphics
+    ) {
         final Level level = mc.level;
         if (level == null) return;
 
         //? < 1.20.5 {
         /*final MapItemSavedData data = MapItem.getSavedData(id, level);
-        *///?} else {
+         *///?} else {
         final MapItemSavedData data = level.getMapData(id);
-         //?}
+        //?}
         if (data == null) return;
 
+        //? < 1.20 {
+        /*final GraphicsHelper graphics = new GraphicsHelper(pose);
+         *///?} else {
         final GraphicsHelper graphics = new GraphicsHelper(guiGraphics);
+        //?}
 
         // Background
         graphics.blit(MAP_SPRITE, x, y, 66, 66);
 
         // Map
         graphics.push();
-        graphics.translate(x + 4, y + 4);
+        graphics.translate(
+                x + 4,
+                y + 4
+                //? < 1.20
+                /*, 401*/
+        );
         graphics.scale(0.45F);
-        //? < 1.21.2 {
+        //? < 1.20 {
+        /*MultiBufferSource.BufferSource immediate = MultiBufferSource.immediate(Tesselator.getInstance().getBuilder());
+        Minecraft.getInstance().gameRenderer.getMapRenderer().render(pose, immediate, id, data, true, 0xf000f0);
+        immediate.endBatch();
+        *///?} else < 1.21.2 {
         /*mc.gameRenderer.getMapRenderer()
                 .render(guiGraphics.pose(), guiGraphics.bufferSource(), id, data, true, 0xf000f0);
         guiGraphics.flush();
